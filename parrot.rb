0695cb375d2eb7f27b6e3cb6d51ee45ae71afd84
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-08 22:34:20 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(sound = "The sound of the parrot is Squawk!")
+def parrot(sound = "Squawk!")
   return "#{sound}"
 end
